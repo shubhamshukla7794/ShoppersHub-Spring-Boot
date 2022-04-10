76439ff6803556f469d09328fac0b1d4cbe6683a
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2022-04-10 12:15:58 +0530

    Initial Commit

---
+++ ShoppersHub/src/main/java/com/shubham/shoppershub/ShoppersHubApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.shoppershub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppersHubApplication.class, args);
+    }
+
+}
---
+++ ShoppersHub/src/test/java/com/shubham/shoppershub/ShoppersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.shoppershub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppersHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
