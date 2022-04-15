9ea92c61182035b3b7e33815f883af792a6d5f8c
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2022-04-15 21:08:59 +0530

    Tweaked AddressController a bit

--- ShoppersHub/shoppershub-web/src/main/java/com/shubham/shoppershub/controller/AddressController.java
+++ ShoppersHub/shoppershub-web/src/main/java/com/shubham/shoppershub/controller/AddressController.java
@@ -6,12 +6,10 @@ import com.shubham.shoppershub.service.AddressService;
 import com.shubham.shoppershub.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/user/{userId}")
 public class AddressController {
     private final UserService userService;
     private final AddressService addressService;
@@ -21,14 +19,13 @@ public class AddressController {
         this.addressService = addressService;
     }
 
-    @GetMapping("/user/{userId}/addresses")
+    @GetMapping("/addresses")
     public String listUserAddresses(@PathVariable String userId, Model model) {
-//        model.addAttribute("user", userService.getUserById(Long.valueOf(userId)));
         model.addAttribute("addressList", addressService.findAllAddressByUserId(Long.valueOf(userId)));
         return "user/address/list";
     }
 
-    @GetMapping("/user/{userId}/address/new")
+    @GetMapping("/address/new")
     public String newUserAddress(@PathVariable String userId, Model model) {
 
         User user = userService.getUserById(Long.valueOf(userId));
@@ -38,22 +35,20 @@ public class AddressController {
         return "user/address/addressForm";
     }
 
-    @PostMapping("/user/{userId}/address")
+    @PostMapping("/address")
     public String saveOrUpdateAddress(@ModelAttribute("address") Address address) {
-//        User user = userService.getUserById(Long.valueOf(userId));
-//        address.setUser(user);
         Address newAddress = addressService.saveAddress(address);
         return "redirect:/user/" + newAddress.getUser().getId() + "/addresses";
     }
 
-    @GetMapping("/user/{userId}/address/{addrId}/update")
+    @GetMapping("/address/{addrId}/update")
     public String updateUserAddress(@PathVariable String userId, @PathVariable String addrId, Model model) {
         Address address = addressService.findByUserIdAndAddrId(Long.valueOf(userId),Long.valueOf(addrId));
         model.addAttribute("address", address);
         return "user/address/addressForm";
     }
 
-    @GetMapping("/user/{userId}/address/{addrId}/delete")
+    @GetMapping("/address/{addrId}/delete")
     public String deleteUserAddress(@PathVariable String userId, @PathVariable String addrId) {
         addressService.deleteAddressById(Long.valueOf(addrId));
         return "redirect:/user/" + userId + "/addresses";
